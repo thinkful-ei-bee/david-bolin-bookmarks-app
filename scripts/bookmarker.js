d27b0e60861b98f7626f4d5b068c85d90af0bc83
David Bolin <david@bolin.at>
2019-03-05 18:45:57 -0800

    added star rating input

--- scripts/bookmarker.js
+++ scripts/bookmarker.js
@@ -55,11 +55,21 @@ const bookmarker = (function(){
       <label for="bookmark-url">url:</label>
       <input type="url" name="url" id="bookmark-url"/>
       <label for="description">Description:</label>
+      <fieldset><legend>Rating</legend>
       <input type="text" name="desc" id="description"/>
-      <!-- need to research how to insert star selector-->
+      <input type="radio", id="1-star", name="rating", value="1">
+      <label for="1-star">&#9733;</label>
+      <input type="radio", id="2-star", name="rating", value="2">
+      <label for="2-star">&#9733;&#9733;</label>
+      <input type="radio", id="3-star", name="rating", value="3">
+      <label for="3-star">&#9733;&#9733;&#9733;</label>
+      <input type="radio", id="4-star", name="rating", value="4">
+      <label for="4-star">&#9733;&#9733;&#9733;&#9733;</label>
+      <input type="radio", id="5-star", name="rating", value="5">
+      <label for="5-star">&#9733;&#9733;&#9733;&#9733;&#9733;</label></fieldset>
       <button id="cancel-btn">Cancel</button>
       <input type="submit" value="Submit" class="submit-new"/>`);
-
+      $('#min-or-add').html('');
     } else {
       $('#new-item-form').html('');
       $('#min-or-add').html(`<button id="dropdown">Minimum Rating: *</button>
